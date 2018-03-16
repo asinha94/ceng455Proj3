c1bcc0f8bcc3d9050f25021b24fa71ef393a9000
Anuraag Sinha <asinha@uvic.ca>
2018-03-15 17:43:40 -0700

    added stuff

---
+++ DDS/Debug/Generated_Code/subdir.mk
@@ -0,0 +1,54 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Generated_Code/Cpu.c \
+../Generated_Code/DD.c \
+../Generated_Code/MainTask.c \
+../Generated_Code/clockMan1.c \
+../Generated_Code/fsl_hwtimer1.c \
+../Generated_Code/fsl_mpu1.c \
+../Generated_Code/hardware_init.c \
+../Generated_Code/mqx_ksdk.c \
+../Generated_Code/osa1.c \
+../Generated_Code/pin_init.c \
+../Generated_Code/uart1.c 
+
+OBJS += \
+./Generated_Code/Cpu.o \
+./Generated_Code/DD.o \
+./Generated_Code/MainTask.o \
+./Generated_Code/clockMan1.o \
+./Generated_Code/fsl_hwtimer1.o \
+./Generated_Code/fsl_mpu1.o \
+./Generated_Code/hardware_init.o \
+./Generated_Code/mqx_ksdk.o \
+./Generated_Code/osa1.o \
+./Generated_Code/pin_init.o \
+./Generated_Code/uart1.o 
+
+C_DEPS += \
+./Generated_Code/Cpu.d \
+./Generated_Code/DD.d \
+./Generated_Code/MainTask.d \
+./Generated_Code/clockMan1.d \
+./Generated_Code/fsl_hwtimer1.d \
+./Generated_Code/fsl_mpu1.d \
+./Generated_Code/hardware_init.d \
+./Generated_Code/mqx_ksdk.d \
+./Generated_Code/osa1.d \
+./Generated_Code/pin_init.d \
+./Generated_Code/uart1.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Generated_Code/%.o: ../Generated_Code/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -O0 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wall  -g3 -D"CPU_MK64FN1M0VLL12" -D"FSL_RTOS_MQX" -D"PEX_MQX_KSDK" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/hal/inc" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/hal/src/sim/MK64F12" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/system/src/clock/MK64F12" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/system/inc" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/osa/inc" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/CMSIS/Include" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/devices" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/devices/MK64F12/include" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/utilities/src" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/utilities/inc" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/devices/MK64F12/startup" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/Generated_Code/SDK/platform/devices/MK64F12/startup" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/Sources" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/Generated_Code" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/drivers/inc" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/rtos/mqx/mqx/source/psp/cortex_m" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/rtos/mqx/mqx/source/psp/cortex_m/cpu" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/rtos/mqx/config/common" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/rtos/mqx/mqx/source/include" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/rtos/mqx/mqx/source/bsp" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/rtos/mqx/mqx/source/psp/cortex_m/compiler/gcc_arm" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/rtos/mqx/mqx/source/nio" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/rtos/mqx/mqx/source/nio/src" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/rtos/mqx/mqx/source/nio/fs" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/rtos/mqx/mqx/source/nio/drivers/nio_dummy" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/rtos/mqx/mqx/source/nio/drivers/nio_serial" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/rtos/mqx/mqx/source/nio/drivers/nio_tty" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/rtos/mqx/mqx_stdlib/source/include" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/rtos/mqx/mqx_stdlib/source/stdio" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/hal/src/uart" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/drivers/src/uart" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/Generated_Code/SDK/rtos/mqx/config/board" -I"C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/Generated_Code/SDK/rtos/mqx/config/mcu" -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ DDS/Debug/SDK/platform/osa/src/fsl_os_abstraction_bm.d
@@ -0,0 +1,68 @@
+SDK/platform/osa/src/fsl_os_abstraction_bm.o: \
+ ../SDK/platform/osa/src/fsl_os_abstraction_bm.c \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/osa/inc/fsl_os_abstraction.h \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/osa/inc/fsl_os_abstraction_bm.h \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/system/inc/fsl_interrupt_manager.h \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/devices/fsl_device_registers.h \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/devices/MK64F12/include/MK64F12.h \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/CMSIS/Include/core_cm4.h \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/CMSIS/Include/core_cmInstr.h \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/CMSIS/Include/core_cmFunc.h \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/CMSIS/Include/core_cmSimd.h \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/Generated_Code/SDK/platform/devices/MK64F12/startup/system_MK64F12.h \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/devices/MK64F12/include/MK64F12_extension.h \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/devices/MK64F12/include/MK64F12.h \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/devices/MK64F12/include/fsl_bitaccess.h \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/devices/MK64F12/include/MK64F12_features.h \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/system/inc/fsl_clock_manager.h \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/hal/inc/fsl_sim_hal.h \
+ c:\users\asinha.uvic.001\documents\ceng455proj3\dds\sdk\platform\hal\src\sim\mk64f12\fsl_sim_hal_mk64f12.h \
+ c:\users\asinha.uvic.001\documents\ceng455proj3\dds\sdk\platform\system\src\clock\mk64f12\fsl_clock_mk64f12.h \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/hal/inc/fsl_mcg_hal.h \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/hal/inc/fsl_mcg_hal_modes.h \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/hal/inc/fsl_mcg_hal.h \
+ C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/hal/inc/fsl_osc_hal.h
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/osa/inc/fsl_os_abstraction.h:
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/osa/inc/fsl_os_abstraction_bm.h:
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/system/inc/fsl_interrupt_manager.h:
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/devices/fsl_device_registers.h:
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/devices/MK64F12/include/MK64F12.h:
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/CMSIS/Include/core_cm4.h:
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/CMSIS/Include/core_cmSimd.h:
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/Generated_Code/SDK/platform/devices/MK64F12/startup/system_MK64F12.h:
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/devices/MK64F12/include/MK64F12_extension.h:
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/devices/MK64F12/include/MK64F12.h:
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/devices/MK64F12/include/fsl_bitaccess.h:
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/devices/MK64F12/include/MK64F12_features.h:
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/system/inc/fsl_clock_manager.h:
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/hal/inc/fsl_sim_hal.h:
+
+c:\users\asinha.uvic.001\documents\ceng455proj3\dds\sdk\platform\hal\src\sim\mk64f12\fsl_sim_hal_mk64f12.h:
+
+c:\users\asinha.uvic.001\documents\ceng455proj3\dds\sdk\platform\system\src\clock\mk64f12\fsl_clock_mk64f12.h:
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/hal/inc/fsl_mcg_hal.h:
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/hal/inc/fsl_mcg_hal_modes.h:
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/hal/inc/fsl_mcg_hal.h:
+
+C:/Users/asinha.UVIC.001/Documents/ceng455Proj3/DDS/SDK/platform/hal/inc/fsl_osc_hal.h:
